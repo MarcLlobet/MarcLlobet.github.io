a9038314aacea98d5266114d766f50995748374b
MarcLlobet <marc.llobet@typeform.com>
2023-01-23 01:12:51 +0100

    add next framework pages 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,13 @@ import { Noto_Serif_Display } from '@next/font/google'
 import { Map, Marker } from "pigeon-maps"
 import GeoJsonData from '@/data/geo.json'
 import { GeoJsonFeatureCollection } from '@/types/map'
-import MllSVG from 'public/mll.svg'
+// import MllSVG from 'public/mll.svg'
 import MarkerSVG from 'public/marker.svg'
 
 const Noto = Noto_Serif_Display()
 
 type HomeProps = {
   mapDetails: GeoJsonFeatureCollection
-}
-
-const MAP_ID = 'd40b681a-4432-49a2-a044-a4c5dd1a239e'
-const MAPTILER_ACCESS_TOKEN = 'I0ZHCXgpFw9I6mZPxwM1' // 'xokEC0yXwkBZMEh6M6w4'
-
-function mapTiler (x, y, z, dpr) {
-  const apiBaseUrl = 'https://api.maptiler.com'
-  const mapsEndpoint = 'maps'
-
-  return `${apiBaseUrl}/${mapsEndpoint}/${MAP_ID}/?key=${MAPTILER_ACCESS_TOKEN}#${z}/${x}/${y}`
-
-  return `${apiBaseUrl}/${mapsEndpoint}/${MAP_ID}/256/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png?key=${MAPTILER_ACCESS_TOKEN}`
-}
-
-const handleMarkerClick = props => {
-  console.log(props, 'click')
-}
-
-const handleMarkerHover = props => {
-  console.log(props, 'hover')
 }
 
 export default function Home({mapDetails}: HomeProps) {
@@ -63,8 +43,6 @@ export default function Home({mapDetails}: HomeProps) {
                 key={properties.name}
                 width={50} 
                 anchor={[lat, lng]}
-                onClick={handleMarkerClick}
-                onMouseOver={handleMarkerHover}
               >
                 <Image
                   src={MarkerSVG}
